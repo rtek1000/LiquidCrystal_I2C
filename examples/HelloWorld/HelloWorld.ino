803f74cd6c1a13bad7db4f5ff14077cfaea2783c
rtek1000 <rh.silicon@gmail.com>
2024-07-30 16:19:47 -0300

    Update HelloWorld.ino

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -3,21 +3,17 @@
 //Library version:1.1
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
 {
   lcd.init();                      // initialize the lcd 
   // Print a message to the LCD.
   lcd.backlight();
-  lcd.setCursor(3,0);
+  lcd.setCursor(1,0); // line 0; row 1
   lcd.print("Hello, world!");
-  lcd.setCursor(2,1);
+  lcd.setCursor(0,1); // line 1; row 0
   lcd.print("Ywrobot Arduino!");
-   lcd.setCursor(0,2);
-  lcd.print("Arduino LCM IIC 2004");
-   lcd.setCursor(2,3);
-  lcd.print("Power By Ec-yuan!");
 }
 
 
